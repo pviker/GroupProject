70200d925982dce089cd5fb16d45a14c67e57142
Patrick Viker <pviker@me.com>
2015-09-30 10:22:19 -0500

    Product page styling

--- amps/productPages/supersonic.php
+++ amps/productPages/supersonic.php
@@ -60,10 +60,12 @@
 
 	
 	<!--START MAIN CONTENT-->
-	<div class="mainContent">		
+	<div class="productContent">		
 			
-			    <p>Fender Super Sonic 22 Watt 1x12" Combo</p>
-			    <p>FEATURES:</p>
+			<span class="productImg"><img src="../../images/amplifiers/Fender_SuperSonic.jpg" height="450" width="450" /></span>
+		    <span>
+		    <p>Fender Super Sonic 22 Watt 1x12" Combo</p>
+		    <p>FEATURES:</p>
 			       <ul>
 				       <li>Deluxe Reverb power platform</li>
 				       <li>Vintage channel contains pre-gain tone controls and two switchable voices</li>
@@ -73,13 +75,13 @@
 				       <li>4-button footswitch for Channel, Fat, Reverb, Effects Loop</li>
 				       <li>Amp Type: Tube</li>
 				       <li>Output: 22 Watts, 8 ohms</li>
-                       <li>Speaker: 12", 50 Watt Fender "Lightning Bolt" Speaker by Eminence</li>
-                       <li>Channels: 2 (Vintage and Burn)</li>
-                       <li>Controls: Vintage Channel: Volume, Treble, Bass, Voicing Switch (Normal/Fat). Burn Channel: Gain 1, Gain 2, Treble, Bass, Middle, Volume, Master Reverb</li>
-                       <li>Effects Spring Reverb</li>
-                  </ul>
-                  
-				<span class="img"><img src="../../images/amplifiers/Fender_SuperSonic.jpg" height="450" width="450" /></span>
+	                   <li>Speaker: 12", 50 Watt Fender "Lightning Bolt" Speaker by Eminence</li>
+	                   <li>Channels: 2 (Vintage and Burn)</li>
+	                   <li>Controls: Vintage Channel: Volume, Treble, Bass, Voicing Switch (Normal/Fat). <br /> Channel: Gain 1, Gain 2, Treble, Bass, Middle, Volume, Master Reverb</li>
+	                   <li>Effects Spring Reverb</li>
+	              </ul>
+			</span>    
+				
 				
 			
 			<!--<tr>

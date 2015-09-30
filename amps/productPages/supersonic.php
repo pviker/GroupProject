d1681dcc8640e5681e6f9da78a5aebda80ace051
pschilmoeller <pschilmoeller@me.com>
2015-09-30 10:03:12 -0500

    Fender Super Sonic
    Product page for Super Sonic

---
+++ amps/productPages/supersonic.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+	<head>
+		
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		
+		<link  rel="stylesheet" type="text/css" href="../../styles/style.css" />
+		<link  rel="stylesheet" type="text/css" href="../../styles/navigation.css" />
+		
+		<title>ICS325 - Group Project - Electric Guitar Amps</title>
+		
+	</head>
+	<body>
+
+<!--START MENU-->
+	<div>
+		<ul id="mainNav">
+			<li><a href="../index.php">home</a></li>
+			
+			<li>
+				<a href="#">guitars</a>
+				<ul>
+					<li><a href="#">acoustics</a></li>
+					<li><a href="../../guitars/electrics.php">electrics</a></li>
+					<li><a href="#">basses</a></li>
+				</ul>
+			</li>
+			
+			<li>
+				<a href="#">amps</a>
+				<ul>
+					<li><a href="#">acoustic</a></li>
+					<li><a href="../electric.php">electric</a></li>
+					<li><a href="#">bass</a></li>
+				</ul>
+			</li>
+			
+			<li>
+				<a href="#">drums</a>
+				<ul>
+					<li><a href="#">acoustic</a></li>
+					<li><a href="#">electric</a></li>
+				</ul>
+			</li>
+			
+			<li>
+				<a href="#">information</a>
+				<ul>
+					<li><a href="#">about</a></li>
+					<li><a href="#">contact</a></li>
+				</ul>
+			</li>
+
+		</ul>
+	</div>
+	<!--END MENU-->	
+	
+	<div class="head">Music Electric Inc.</div>
+
+	
+	<!--START MAIN CONTENT-->
+	<div class="mainContent">		
+			
+			    <p>Fender Super Sonic 22 Watt 1x12" Combo</p>
+			    <p>FEATURES:</p>
+			       <ul>
+				       <li>Deluxe Reverb power platform</li>
+				       <li>Vintage channel contains pre-gain tone controls and two switchable voices</li>
+				       <li>Burn channel contains dual gain controls and post-gain EQ</li>
+				       <li>Fender long-spring Reverb by Accutronics</li>
+				       <li>Ivory "radio" knobs</li>
+				       <li>4-button footswitch for Channel, Fat, Reverb, Effects Loop</li>
+				       <li>Amp Type: Tube</li>
+				       <li>Output: 22 Watts, 8 ohms</li>
+                       <li>Speaker: 12", 50 Watt Fender "Lightning Bolt" Speaker by Eminence</li>
+                       <li>Channels: 2 (Vintage and Burn)</li>
+                       <li>Controls: Vintage Channel: Volume, Treble, Bass, Voicing Switch (Normal/Fat). Burn Channel: Gain 1, Gain 2, Treble, Bass, Middle, Volume, Master Reverb</li>
+                       <li>Effects Spring Reverb</li>
+                  </ul>
+                  
+				<span class="img"><img src="../../images/amplifiers/Fender_SuperSonic.jpg" height="450" width="450" /></span>
+				
+			
+			<!--<tr>
+				<td><span class="img"><img src="../images/amplifiers/Marshall_bluesbreaker.jpg" height="200" width="200" /></span></td>
+				<td>Marshall 1962 Bluesbreaker - Used</td>
+				<td>$1399</td>
+			</tr> -->
+			
+	</div>
+	<!--END MAIN CONTENT-->
+
+	</body>
+</html>

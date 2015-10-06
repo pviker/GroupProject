aa1a90411359edf1dacdf1304cb7fa4f04d2ade2
Patrick Viker <pviker@me.com>
2015-10-06 10:54:55 -0500

    Include file experimenting
    I was able to setup an include file for the menu so we only have to
    make changes to that in one spot.

---
+++ index2.php
@@ -0,0 +1,21 @@
+<?php
+  require("navigation.inc");
+ 
+  $navigation = new Navigation();
+
+  echo $navigation;
+?>
+
+		<div class="head">Music Electric Inc.</div>
+	
+		<div class="mainContent" >
+			<img src="images/mainguitar.jpg" id="mainPageImg"/><br />
+			<p id="indexContent">
+				<h1 class="indexH1">Welcome to Music Electric Inc!</h1> <br /><br />
+				<h3 class="indexH1">We sell beginner to professional level musical instruments at a competitive price.
+				Check out our inventory and shoot us an email with any questions!</h3>
+			</p>
+		</div>
+		
+	</body>
+</html>

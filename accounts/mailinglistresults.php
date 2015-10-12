cefcf3a289dca855047cf0fb952edae6d191fc85
pschilmoeller <pschilmoeller@me.com>
2015-10-12 08:50:52 -0500

    Database updates

--- accounts/mailinglistresults.php
+++ accounts/mailinglistresults.php
@@ -19,8 +19,25 @@
 ?>
 
 <?php 
+
+    //USERS table data
+    $firstName = $_POST['FIRST'];
+    $lastName = $_POST['LAST'];
+    $date = $_POST['DATE'];
+    $email = $_POST['EMAIL'];
+    $dob = $_POST['DOB'];
+    $address = $_POST['ADDRESS'];
+    $city = $_POST['LOCALITY'];
+    $state = $_POST['REGION'];
+    $zip = $_POST['POSTAL'];
+    $phone = $_POST['PHONE'];
     
-    echo "<div class=\"mainContent\"><h1>" . $_POST['FIRST'] . "</h1></div>";
+    //CREDENTIALS table data
+    $username = $_POST['USERNAME'];
+    $password = $_POST['PASSWORD'];
+   
+    
+    //echo "<div class=\"mainContent\"><h1>" . $_POST['FIRST'] . "</h1></div>";
     
     @ $db = mysqli_connect('localhost', 'user1', 'abc123', 'music_electric');
     
@@ -29,12 +46,24 @@
                 exit;
             }
     
-    $query = "insert into users (first_name, last_name) values ('" . $_POST['FIRST'] . "', '" . 
-    $_POST['LAST'] . "')";
+    $query = "insert into users (first_name, last_name, date, email, dob, address, city, state, 
+    zip, phone ) values ('" . $firstName . "', '" . $lastName . "', '" . $date . "', '" . $email . "', '" . $dob 
+    . "', '" . $address . "', '" . $city . "', '" . $state . "', '" . $zip . "', '" . $phone . "')";
     
-    $result = mysqli_query($db, $query);
+    if (mysqli_query($db, $query)) {
+        
+    $last_id = mysqli_insert_id($db);
+        
+    echo "New record created successfully. Last inserted ID is: " . $last_id;
     
-    mysqli_free_result($result);
+    } else {
+        
+    echo "Error: could not complete query.";
+    
+    }
+    
+    echo $last_id;
+    
     mysqli_close($db);
 	
 	

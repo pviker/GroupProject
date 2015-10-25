0383e5e00557da51f0ebaacc69393af6da10906e
Patrick Viker <pviker@me.com>
2015-10-25 11:28:42 -0500

    product details page

--- productDetails.php
+++ productDetails.php
@@ -10,6 +10,10 @@
  *   
  * */
  
+ 	global $id;
+	global $cat;
+	global $subcat;
+ 
  	if(null === session_id()){
 		session_start();
 	}
@@ -27,6 +31,10 @@
 		$id = $_GET["id"];
 	} else $id = "";
 	
+	if (isset($_GET["prod_id"])){
+		$prod_id = $_GET["prod_id"];
+	} else $prod_id = "";
+	
 	if (isset($_GET["cat"])){
 		$cat = $_GET["cat"];
 	} else $cat = "";
@@ -35,19 +43,36 @@
 		$subcat = $_GET["subcat"];
 	} else $subcat = "";
     
- ?>
+ ?>					
  
 	<div class="breadcrumb">    
           <ul>
             <li><a href="index.php">home</a></li>
-            <li><a href="products.php?cat=<?php echo $_GET["cat"]; ?>"><?php echo $_GET["cat"]; ?></a></li>
-            <?php if (isset($_GET["subcat"])){ ?>
-            	<li><a href=""><?php echo $_GET["subcat"]; ?></a></li> 
-            <?php } ?>         
+            <li><a href="products.php?cat=<?php echo $cat; ?>"><?php echo $cat; ?></a></li>
+            <li><<a href="products.php?id=<?php echo $id ?>&cat=<?php echo $cat; ?>&subcat=<?php echo $subcat ?>"><?php echo $subcat; ?></a></li> 
+            <li><a href="">Details</a></li>
+                     
           </ul>
     </div>
     
-    <div class = "mainContent">
+    <div class = "productContent">
+    	
+    	
+    <!--START MAIN CONTENT-->		
+			<p><h2>Marshall 1962 Bluesbreaker - Used</h2></p>
+			<span class="productImg"><img src="images/amplifiers/Marshall_bluesbreaker.jpg" height="450" width="450" /></span>
+		    <span>
+				<p>$1399</p>
+				<p>FEATURES:</p>
+					<ul>
+						<li>30W output</li>
+						<li>2 - 12" 25W Celestion Greenback speakers</li>
+						<li>Valve rectifier</li>
+					</ul>
+			</span>	
+
+	<!--END MAIN CONTENT-->
+    	
     	
      	<h1>Products page in progress...</h1> <br/>
      	
@@ -69,25 +94,23 @@
      			} else {
      					$query = "select * from products where cat_id = " . $id;
 				  }
+				
 			$results = mysqli_query($dbc, $query);	
      	
 			// echo all products matching category ID
 			while($row = mysqli_fetch_assoc($results)) {    
 	        	echo "<tr>
-	        			<td><a href=\"/ics325/GroupProject/productDetails.php?prod_id=" . 
-	        						$row["prod_id"] . "&id=" . $id . "&cat=" . $cat . "&subcat=" . $subcat . "\">
-	        					<span class=\"img\">
-	        						<img src=\"" . $row["photo_loc"] . "\" height=\"200\" width=\"200\" />
-	        					</span>
-	        				</a></td>
-	        			<td><a href=\"/ics325/GroupProject/products.php?prod_id=" . 
-	        						$row["prod_id"] . "&id=" . $id . "&cat=" . $cat . "&subcat=" . $subcat . "\">" . $row["title"] . "
-	        				</a></td>
+	        			<td><a href=\"/ics325/GroupProject/products.php?prod_id=" . $row["prod_id"] . "\">
+	        					<span class=\"img\"><img src=\"" . $row["photo_loc"] . "\" height=\"200\" width=\"200\" /></span</a></td>
+	        			<td><a href=\"/ics325/GroupProject/products.php?prod_id=" . $row["prod_id"] . "\">" . $row["title"] . "</a></td>
 	        			<td>" . $row["price"] . "</td>    
        				  </tr>";
 			} // end while
 			
         ?>
+        
+
+        
 		</table>
 				
     </div>

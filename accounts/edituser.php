6a7087ebd05d7e4f3f30c4de2e86cb67a7b0f080
pschilmoeller <pschilmoeller@me.com>
2015-10-27 10:05:22 -0500

    Search for user

--- accounts/edituser.php
+++ accounts/edituser.php
@@ -19,15 +19,38 @@
     
     require("../controllers/database.php");
     
+    if(isset($_POST['search'])) {
+        
+        $usernameSearch = $_POST['search'];
+        
+    }
+    
+    $userQuery = "select credentials.username, first_name, last_name, email, dob, address, city, state, 
+    zip, phone, gender, credentials.admin from users, credentials where users.userid = credentials.userid and 
+    username = '" . $usernameSearch . "'";
+    
+    $result = mysqli_query($dbc, $userQuery);
+    
+    while($row = mysqli_fetch_assoc($result)) {
+        
+        $username = $row['username'];
+        $fname = $row['first_name'];
+        $lname = $row['last_name'];
+        $email = $row['email'];
+        $dob = $row['dob'];
+        $address = $row['address'];
+        $city = $row['city'];
+        $state = $row['state'];
+        $zip = $row['zip'];
+        $phone = $row['phone'];
+        $gender = $row['gender'];
+        $admin = $row['admin'];
+        
+    }
     
     
 ?>
 
-<!DOCTYPE html>
-
-<html>
-    
-    <body>
         
         <div class="breadcrumb">
         <nav>
@@ -64,40 +87,40 @@
             <fieldset id = "field1">
                 
                <label>Username:</label>
-                   <input type="text" name="userName" size="40"><br>
+                   <input type="text" name="userName" readonly="true" value = "<?php echo $username ?>" size="40"><br>
                    
                <label>First Name:</label>
-                   <input type="text" name="fName" size="40"><br>
+                   <input type="text" name="fName" value = "<?php echo $fname ?>" size="40"><br>
             
                <label>Last Name:</label>
-                   <input type="text" name="lName" size="40"><br>
+                   <input type="text" name="lName" value = "<?php echo $lname ?>" size="40"><br>
                 
                <label>E-mail:</label>
-                   <input type="text" name="email" size="40"><br>
+                   <input type="text" name="email" value = "<?php echo $email ?>" size="40"><br>
                    
                <label>Date of Birth:</label>
-                   <input type="text" name="dob" size="15"><br>
+                   <input type="text" name="dob" value = "<?php echo $dob ?>" size="15"><br>
                    
                <label>Address:</label>
-                   <input type="text" name="address" size="40"><br>
+                   <input type="text" name="address" value = "<?php echo $address ?>" size="40"><br>
                    
                <label>City:</label>
-                   <input type="text" name="city" size="40"><br>
+                   <input type="text" name="city" value = "<?php echo $city ?>" size="40"><br>
                    
                <label>State:</label>
-                   <input type="text" name="state" size="5"><br>
+                   <input type="text" name="state" value = "<?php echo $state ?>" size="5"><br>
                    
                <label>Zip Code:</label>
-                   <input type="text" name="zip" size="10"><br>
+                   <input type="text" name="zip" value = "<?php echo $zip ?>" size="10"><br>
                    
                <label>Phone Number:</label>
-                   <input type="text" name="phone" size="15"><br>
+                   <input type="text" name="phone" value = "<?php echo $phone ?>" size="15"><br>
                    
                <label>Gender:</label>
-                   <input type="text" name="gender" size="10"><br>
+                   <input type="text" name="gender" value = "<?php echo $gender ?>" size="10"><br>
                    
                <label>Admin:</label>
-                   <input type="text" name="lName" size="5"><br>
+                   <input type="text" name="admin" value = "<?php echo $admin ?>" size="5"><br>
             
             </fieldset>
             

9d35e6ed4bf1562ece2e99689f71b5a7cbccbc6c
pschilmoeller <pschilmoeller@me.com>
2015-11-10 08:32:31 -0600

    View All Orders
    So I’ve got the view all orders page complete. You’re able to view all
    of the orders in a table and then you can click on the view products
    link in that table to see all of the product info for each order.

---
+++ orders/viewAllOrders.php
@@ -0,0 +1,92 @@
+<?php 
+
+
+/* 
+ * ICS325 - Group Project
+ * Iteration: 5
+ * Group: D for Dolphins
+ * File: userinfo.php
+ * Author: Kevin Casey, Jordan Grenier, Paul Schilmoeller, Patrick Viker, Joshua Wilson
+ * Description: This page displays all orders to admin
+ *   
+ * */
+
+
+if(null === session_id()){
+        session_start();
+    }
+
+    require("../navigation.inc");
+    $navigation = new Navigation();
+    echo $navigation;
+    
+    require("../controllers/database.php");
+    
+    if ($_SESSION['adminFlag'] !== 1) {
+        header ('Location: login.php'); 
+    }
+
+?>
+
+
+<div class="breadcrumb">    
+          <ul>
+            <li><a href="../index.php">home</a></li>
+            <li><a href="../accounts/admin.php">admin</a></li> 
+            <li><a href="">view all orders</a></li>           
+          </ul>
+    </div>
+    
+
+<div class = "mainContentTable">
+        
+        <h1 class="indexH1">All Orders</h1>
+         
+         <table class = "cartTable">
+             <tr>
+                 <td>Order ID</td>
+                 <td>First Name</td>
+                 <td>Last Name</td>
+                 <td>Order Date</td>
+                 <td>Total Amount</td>
+                 <td>Products</td>
+         </tr>
+          
+<?php 
+
+      $selectOrdersQuery = "select * from orders";
+      
+      $orderResults = mysqli_query($dbc, $selectOrdersQuery);
+      
+      while($orderRow = mysqli_fetch_assoc($orderResults)) {
+          
+          $userQuery = "select first_name, last_name from users where users.userid = '" . $orderRow['user_id'] . "'";
+          
+          $userResults = mysqli_query($dbc, $userQuery);
+          
+          $userRow = mysqli_fetch_assoc($userResults);
+          
+          
+          
+      echo "<tr>
+      
+                <td>" . $orderRow['orders_id'] . "</td>
+                <td>" . $userRow['first_name'] . "</td>
+                <td>" . $userRow['last_name']  . "</td>
+                <td>" . $orderRow['date']      . "</td>
+                <td>" . $orderRow['amount']    . "</td>
+                <td><a href=\"viewOrderProducts.php?orderid=" . $orderRow['orders_id'] . "\" style=\"color:black\"\">View Products</a></td>
+                
+           </tr>";
+                
+      }
+      
+?>         
+          
+ </table>
+ 
+</div>
+
+</body>
+
+</html>
---
+++ orders/viewOrderProducts.php
@@ -0,0 +1,96 @@
+<?php 
+
+/* 
+ * ICS325 - Group Project
+ * Iteration: 5
+ * Group: D for Dolphins
+ * File: userinfo.php
+ * Author: Kevin Casey, Jordan Grenier, Paul Schilmoeller, Patrick Viker, Joshua Wilson
+ * Description: This page displays all products for an order
+ *   
+ * */
+
+
+if(null === session_id()){
+        session_start();
+    }
+
+    require("../navigation.inc");
+    $navigation = new Navigation();
+    echo $navigation;
+    
+    require("../controllers/database.php");
+    
+    if ($_SESSION['adminFlag'] !== 1) {
+        header ('Location: login.php'); 
+    }
+
+?>
+
+     <div class="breadcrumb">    
+          <ul>
+            <li><a href="../index.php">home</a></li>
+            <li><a href="../accounts/admin.php">admin</a></li> 
+            <li><a href="viewAllOrders.php">view all orders</a></li>
+            <li><a href="">view products in order</a></li>           
+          </ul>
+     </div>
+     
+     <div class = "mainContentTable">
+        
+        <h1 class="indexH1">Products</h1>
+         
+         <table class = "cartTable">
+             <tr>
+                 <td>Product</td>
+                 <td>Price</td>
+                 <td>Quantity</td>
+                 
+             </tr>
+
+<?php
+
+    $orderID = $_GET['orderid'];
+
+    $productInfoQuery = "select title, price from products where prod_id in 
+    (select prod_id from orders, order_items where orders.orders_id = order_items.order_id and orders.orders_id ='" . $orderID . "')";
+
+    $productResults = mysqli_query($dbc, $productInfoQuery);
+    
+    $qtyQuery = "select qty from orders, order_items where orders.orders_id=order_items.order_id and orders.orders_id='" . $orderID . "'";
+    
+    $qtyResults = mysqli_query($dbc, $qtyQuery);
+    
+    while($productRow = mysqli_fetch_assoc($productResults)) {
+        
+        echo "<tr>
+        
+                <td>" . $productRow['title'] . "</td>
+                <td>" . $productRow['price']  . "</td>";
+                
+         $qtyRow = mysqli_fetch_assoc($qtyResults);
+         
+         foreach($qtyRow as $qty) {
+             
+             echo "<td>" . $qty . "</td>";             
+             
+         }
+             
+             
+                
+        echo "</tr>";        
+           
+     }
+
+
+?>
+
+</table>
+
+</div>
+
+</body>
+
+</html>
+
+
